3f8ecc34fe03f92f54bbf23da69a0c0731ad2438
Leah Mattern <ladyleahmattern@gmail.com>
2021-03-24 16:23:57 -0400

    Routing like a boss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ApplicationViewComponent } from './application-view/application-view.component';
 import { RegisterModalComponent } from './register-modal/register-modal.component';
-
+import { SignInComponent } from './sign-in/sign-in.component';
 
 const routes: Routes = [
-    { path: 'register', component: RegisterModalComponent },
-    { path: 'application-view', component: ApplicationViewComponent }
+  { path: '', component: SignInComponent },
+  { path: 'register', component: RegisterModalComponent },
+  { path: 'application-view', component: ApplicationViewComponent }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-
 export class AppRoutingModule { }
+export const routingComponents = [SignInComponent, RegisterModalComponent, ApplicationViewComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SignInComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
